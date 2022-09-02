8e82f0586d291f868651057e58e94276f8e6266a
RodrigoFerrao <rodrigo.ferrao@ufba.br>
2022-09-02 12:47:13 -0300

    adicionando configuração no cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "jkuavf",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
