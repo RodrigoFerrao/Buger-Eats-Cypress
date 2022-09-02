e301ce55b4670f45f72964289ef742b5ebaad3f3
RodrigoFerrao <rodrigo.ferrao@ufba.br>
2022-09-02 11:18:53 -0300

    Automação da pagina Buger-Eats

---
+++ cypress/Pages/SignupPage.js
@@ -0,0 +1,57 @@
+class SignupPage{
+
+  browserSettings(){
+  
+  cy.visit('https://buger-eats.vercel.app/')
+  cy.viewport(1440, 900)
+// Assert para identificar o carregamento da página
+  cy.get('div [class="content"] main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+  cy.get('img[alt="Buger Eats"]').should('be.visible')
+  cy.get('a[href="/deliver"]').should('be.visible')   
+ 
+  cy.get('a[href="/deliver"]').click()   
+
+
+}
+  fillform(courier){
+  // Assert que o usuario se encontra na página correta.
+  cy.get('div[id="page-deliver"] form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    // Dados do courier
+    cy.get('input[name="name"]').type(courier.name);
+    cy.get('input[name="cpf"]').type(courier.cpf);
+    cy.get('input[name="email"]').type(courier.email);
+    cy.get('input[name="whatsapp"]').type(courier.whatsapp);
+
+    // Endereço do courier
+    cy.get('input[name="postalcode"]').type(courier.address.postalcode);
+    cy.get('input[type="button"]').click();
+    cy.get('input[name="address-number"]').type(courier.address.number);
+    cy.get('input[name="address-details"]').type(courier.address.address_details);
+   
+    // Validando endereço do courier
+    cy.get('input[name="address"]').should('have.value', courier.address.street);
+    cy.get('input[name="district"]').should('have.value', courier.address.district);
+    cy.get('input[name="city-uf"]').should('have.value', courier.address.city_state);
+    cy.get('input[name="postalcode"]').should('have.value', courier.address.postalcode);
+
+    // Escolhendo metodo de entrega
+    cy.get('li [src="/static/media/moto.c7bfc5a6.svg"]').click();
+   
+    // Fazendo upload da CNH
+    cy.get('div [class ="dropzone"] input[type="file"]').attachFile('/imagens/' + courier.cnh)
+  }
+  submit(){
+    cy.get('button[class="button-success"]').click()
+  }
+  modalValidation(expectedMessage){
+    // Validando Sucesso no teste    
+    cy.get('div[class="swal2-html-container"]').should('have.text', expectedMessage);
+  }
+  alertMessage(expectedMessage){
+    // cy.get('div [class="alert-error"]').should('have.text', expectedMessage)
+    cy.contains('div [class="alert-error"]', expectedMessage).should('be.visible')
+  }
+}
+
+export default new SignupPage;
---
+++ cypress/e2e/buger_Eats.cy.js
@@ -0,0 +1,61 @@
+import SignupPage from "../Pages/SignupPage";
+import signup from "../Pages/SignupPage";
+
+describe("Home Page", () => {
+  it("Dado que app está online", () => {
+    signup.browserSettings();
+  });
+});
+
+describe("Cadastro", () => {
+  beforeEach(function () {
+    cy.fixture("courier.json").then((massa_entregador) => {
+      this.courier = massa_entregador;
+    });
+  });
+
+  it("Usuario deve se tornar um entregador", function () {
+    // instanciando a classe signupPage
+    signup.browserSettings();
+    signup.fillform(this.courier.cadastro_Sucesso);
+    signup.submit();
+    const expectedMessage =
+      "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.";
+    signup.modalValidation(expectedMessage);
+  });
+
+  it("Dado CPF invalido usuario não deve se tornar um entregador", function () {
+    signup.browserSettings();
+    signup.fillform(this.courier.cpf_Invalido);
+    signup.submit();
+    signup.alertMessage("Oops! CPF inválido");
+  });
+
+  it("Dado e-mail invalido usuario não deve se tornar um entregador", function () {
+    signup.browserSettings();
+    signup.fillform(this.courier.email_Invalido);
+    cy.get('input[name="email"]').should("not.contains.value", "@");
+  });
+
+  context("Dado campos ausentes usuario não deve se tornar um entregador", function () {
+    const messages = [
+      { field: "name", output: "É necessário informar o nome" },
+      { field: "cpf", output: "É necessário informar o CPF" },
+      { field: "email", output: "É necessário informar o email" },
+      { field: "postalcode", output: "É necessário informar o CEP" },
+      { field: "number", output: "É necessário informar o número do endereço" },
+      { field: "delivery_method", output: "Selecione o método de entrega" },
+      { field: "cnh", output: "Adicione uma foto da sua CNH" },
+    ]
+
+    before (function(){
+      signup.browserSettings()
+      signup.submit()
+    })
+      messages.forEach(function(msg){
+        it(`${msg.field} is mandatory`, function(){
+          signup.alertMessage(msg.output)
+        })
+      })
+  })
+});
